e13fbf6df25aba045839a3324dcd24606dbd35d9
YoBunnyTiger <157778892+YoBunnyTiger@users.noreply.github.com>
2024-07-08 19:33:09 +0300

    Add files via upload

---
+++ modul_2_2.py
@@ -0,0 +1,30 @@
+print('Ввод в консоль 1:')
+first = input()
+second = input()
+third = input()
+print()
+
+if first == second == third:
+    print('Вывод на консоль 1:\n3')
+
+elif first == second or second == third or first == third:
+    print('Вывод на консоль 1:\n2')
+
+else:
+    print('Вывод на консоль 1:\n0')
+print()
+
+print('Ввод в консоль 2:')
+first = input()
+second = input()
+third = input()
+print()
+
+if first == second == third:
+    print('Вывод на консоль 1:\n3')
+
+elif first == second or second == third or first == third:
+    print('Вывод на консоль 1:\n2')
+
+else:
+    print('Вывод на консоль 1:\n0')
